3816991ec0e42e68d674dc01203863337615c131
Hakuron <pastorino.facundo@gmail.com>
2022-06-17 20:11:10 -0300

    merge store

--- src/main/java/com/example/persistidorobjetos/examples/Persona1.java
+++ src/main/java/com/example/persistidorobjetos/examples/Persona1.java
@@ -12,20 +12,4 @@ public class Persona1
 	private int dni;
 	@Getter @Setter
 	private String nombre;
-	
-	
-	public int getDni() {
-		return dni;
-	}
-	public void setDni(int dni) {
-		this.dni = dni;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-	
 }
--- src/main/java/com/example/persistidorobjetos/model/Atributo.java
+++ src/main/java/com/example/persistidorobjetos/model/Atributo.java
@@ -1,16 +1,18 @@
 package com.example.persistidorobjetos.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+import static javax.persistence.GenerationType.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
-
-import javax.persistence.*;
-
-import static javax.persistence.GenerationType.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -29,31 +31,4 @@ public class Atributo {
     @ManyToOne
 	@JoinColumn(name = "clase_id")
     private Clase clase;
-    
-    
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public TipoAtributo getTipoAtributo() {
-		return tipoAtributo;
-	}
-	public void setTipoAtributo(TipoAtributo tipoAtributo) {
-		this.tipoAtributo = tipoAtributo;
-	}
-	public Clase getClase() {
-		return clase;
-	}
-	public void setClase(Clase clase) {
-		this.clase = clase;
-	}
-	
 }
